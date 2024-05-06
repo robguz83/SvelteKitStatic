52886a1cae5d512c4aca5873e0bb8dcf240b072d
Roberto Guzman <robguz83@users.noreply.github.com>
2024-05-06 18:51:19 +0000

    Add Page ejemplo
    Se agrega la pagina de ejemplo

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,6 +25,9 @@
 			<li aria-current={$page.url.pathname === "/posts" ? "page" : undefined}>
 				<a href="/posts">Posts</a>
 			</li>
+			<li aria-current={$page.url.pathname === "/ejemplo" ? "page" : undefined}>
+				<a href="/ejemplo">Ejemplo</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ src/routes/ejemplo/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  //export let data;
+  const title = "Esta es una paginas de ejemplo";
+</script>
+
+<svelte:head>
+  <title>Ejemplo</title>
+  <meta name="description" content="Pagina de ejemplo" />
+</svelte:head>
+
+<div class="text-column">
+  <h1>{title}</h1>
+
+  <pre>ejecutando...</pre>
+</div>
