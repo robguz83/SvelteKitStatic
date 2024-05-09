c904f3098bf9f8a729df036c4ec18cc6eadad8ec
Roberto Guzman <robguz83@users.noreply.github.com>
2024-05-09 15:02:50 +0000

    se configuro y agrego los archivos coorespondientes al head

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,33 @@ const config = {
       strict: true,
     }),*/
     adapter: adapter({ fallback: '404.html' }),
+    head: {
+      title: 'Mi Aplicación SvelteKit', // Título de la página
+      // Metadatos
+    meta: [
+        {
+          name: 'description',
+          content: 'Una increíble aplicación construida con SvelteKit',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0',
+        },
+      ],
+      // Enlaces a hojas de estilo
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'styles/global.css',
+        },
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          href: 'favicon.ico',
+        },
+      ],
+    },
+
 	},
 };
 
